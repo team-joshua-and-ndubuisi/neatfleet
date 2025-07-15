a8b35baf18c5db21e99342fc5059a36cc279ad88
Andrew Smith <meuzishun@gmail.com>
2025-07-15 17:48:23 -0400

    Include index.css in main.tsx to get tailwind to work

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App.tsx';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
