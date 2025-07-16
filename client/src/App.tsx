70a10c8b96df0d1c2a27d868bc552c90ca252d10
Andrew Smith <meuzishun@gmail.com>
2025-07-15 23:30:26 -0400

    Adjust App and main to remove MUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import { Provider as StoreProvider } from 'react-redux';
 import store from '@/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from '@/theme';
 import AppRoutes from '@/routes';
 
 const queryClient = new QueryClient();
@@ -11,10 +9,7 @@ function App() {
   return (
     <StoreProvider store={store}>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <AppRoutes />
-        </ThemeProvider>
+        <AppRoutes />
       </QueryClientProvider>
     </StoreProvider>
   );
