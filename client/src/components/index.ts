f6cf2c4a330d864a8e771fd583c8b7f87b3f6ca8
Yassahr <yassahr@gmail.com>
2025-07-17 00:57:54 -0400

    feat: profile components added

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,1 @@
 export * from './ui';
-export { default as ErrorComponent } from './ErrorComponent';
-export { default as LoadingIndicator } from './LoadingIndicator';
-export { default as NavBar } from './NavBar';
-export { default as Spinner } from './Spinner';
