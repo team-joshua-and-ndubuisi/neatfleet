a7d5b30fd7c7fbf5261fb0feebae8fb86ef81605
jesusdoza <jesus.doza@gmail.com>
2025-07-20 18:06:17 -0500

    add util to create store that is persisted to local storage

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { create, StoreApi, UseBoundStore } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
+//to handle combining class names with tailwind and clsx
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+//to create a local store with zustand and persist it to localStorage by passing store instance and store name
+export function createLocalStore<T>(
+  store: UseBoundStore<StoreApi<T>>,
+  storeName: string
+) {
+  return create(
+    persist(store, {
+      name: storeName,
+      storage: createJSONStorage(() => localStorage),
+    })
+  );
 }
