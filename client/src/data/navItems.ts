454b4326a0d5a38a4b3e6086f647a2e60bd1c824
Andrew Smith <meuzishun@gmail.com>
2025-07-21 13:20:48 -0400

    Add services route to navItems

--- client/src/data/navItems.ts
+++ client/src/data/navItems.ts
@@ -1,7 +1,8 @@
 export const navItems = [
-  { text: "Home", path: "/" },
-  { text: "About", path: "/about" },
-  { text: "Contact", path: "/contact" },
-  { text: "Login", path: "/login" },
-  { text: "Signup", path: "/signup" },
+  { text: 'Home', path: '/' },
+  { text: 'About', path: '/about' },
+  { text: 'Contact', path: '/contact' },
+  { text: 'Login', path: '/login' },
+  { text: 'Signup', path: '/signup' },
+  { text: 'Services', path: '/service-catalog' },
 ];
