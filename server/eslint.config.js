6eb86a047122cc99e90436a6e93d48b3f0e8f36d
Andrew Smith <76052265+meuzishun@users.noreply.github.com>
2025-07-21 07:36:20 -0400

    TJN-33-create-prettier-config (#22)
    * Install prettier dev dep in client
    
    * Create prettier rc file
    
    * Add prettierignore file
    
    * Add formatting scripts to package.json
    
    * Add documentation for installing prettier extension and format on save
    
    * Install eslint-config-prettier and add to eslint.config
    
    * Add vscode extensions and settings files to root
    
    * Remove .vscode from client gitignore
    
    * Add .vscode dir and exceptions to root gitignore
    
    * Add jsxSingleQuote and other React settings
    
    * Create prettierrc file for server dir
    
    * Create prettierignore file
    
    * Create eslint config for server
    
    * Install prettier and linter in server and add formatting scripts
    
    * Add 'server' to vscode settings

---
+++ server/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules', 'generated'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
+    files: ['**/*.{ts,js}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  }
+);
