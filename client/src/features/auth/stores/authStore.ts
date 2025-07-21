5986a5e32997b331fd58316e9dfbab350a8e76fc
jesusdoza <jesus.doza@gmail.com>
2025-07-20 19:04:36 -0500

    clean up old persist store

--- client/src/features/auth/stores/authStore.ts
+++ client/src/features/auth/stores/authStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { AuthResponseT, UserProfileT } from "@/features/auth/";
-
+import { createLocalStore } from "@/lib/utils";
 interface AuthStore extends AuthResponseT {
   token: string;
   user: {
@@ -14,7 +14,7 @@ interface AuthStore extends AuthResponseT {
   initAuth: (auth: AuthResponseT) => void;
 }
 
-export const useAuthStore = create<AuthStore>((set) => ({
+const authStore = create<AuthStore>((set, get) => ({
   token: "",
   user: {
     firstName: "",
@@ -39,3 +39,39 @@ export const useAuthStore = create<AuthStore>((set) => ({
     }));
   },
 }));
+
+// good one
+// export const useAuthStore = create(
+//   persist<AuthStore>(
+//     (set, get) => ({
+//       token: "",
+//       user: {
+//         firstName: "",
+//         lastName: "",
+//         isAdmin: false,
+//         id: "",
+//         email: "",
+//       },
+//       setUserToken: (token: string) => set(() => ({ token })),
+//       setUser: (user: UserProfileT) => set(() => ({ user })),
+
+//       initAuth: (auth: AuthResponseT) => {
+//         set(() => ({
+//           token: auth.token,
+//           user: {
+//             id: auth.user.id,
+//             firstName: auth.user.firstName,
+//             lastName: auth.user.lastName,
+//             email: auth.user.email,
+//             isAdmin: auth.user.isAdmin,
+//           },
+//         }));
+//       },
+//     }),
+//     { name: "authStore", storage: createJSONStorage(() => localStorage) }
+//   )
+// );
+
+export const useAuthStore = () => {
+  return createLocalStore<AuthStore>(authStore, "authStore");
+};
