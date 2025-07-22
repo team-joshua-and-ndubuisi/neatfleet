4904c6cbb61d83d95de480acbb143ca1c2ce0a4e
jesusdoza <jesus.doza@gmail.com>
2025-07-22 12:33:21 -0500

    add handle time click function client/src/components/technician/TechAvailabilityForm.tsx

--- client/src/components/technician/TechAvailabilityForm.tsx
+++ client/src/components/technician/TechAvailabilityForm.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import AvailableDayPicker from './AvailableDayPicker';
 import AvailableTimePicker from './AvailableTimePicker';
+import { Button } from '../ui';
 
 export default function TechAvailabilityForm() {
+  const currentDate = new Date();
+  const timeRange = {
+    start: new Date(currentDate),
+    end: new Date(new Date().setHours(currentDate.getHours() + 8)), // 8 hours later,
+  };
+
+  const handleTimeClick = (time: Date) => {
+    console.log('time click', time.toLocaleTimeString());
+  };
+
   return (
     <section>
-      <section>
-        <h2>Set Your Availability</h2>
-      </section>
-      <section>
-        <h3>Select days available</h3>
-        <AvailableDayPicker />
-      </section>
-      <section>
-        <h3>Select time available</h3>
-        <AvailableTimePicker />
-      </section>
+      <form action=''>
+        <section>
+          <h2>Set Your Availability</h2>
+        </section>
+        <section>
+          <h3>Select days available</h3>
+          {/* <AvailableDayPicker selectedDate={currentDate} /> */}
+        </section>
+        <section>
+          <h3>Select time available</h3>
+          <AvailableTimePicker clickCallback={handleTimeClick} range={timeRange} />
+        </section>
+
+        <Button>Confirm availability</Button>
+      </form>
     </section>
   );
 }
