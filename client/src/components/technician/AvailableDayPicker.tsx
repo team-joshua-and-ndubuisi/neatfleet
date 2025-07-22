08a6081ed1398921549e44427599e3999fef207f
jesusdoza <jesus.doza@gmail.com>
2025-07-22 10:52:10 -0500

    add mock data to client/src/components/technician/AvailableDayPicker.tsx and client/src/components/technician/AvailableTimePicker.tsx

--- client/src/components/technician/AvailableDayPicker.tsx
+++ client/src/components/technician/AvailableDayPicker.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 
+type DayT = {
+  value: string;
+  label: string;
+};
+
+const daysOfWeek: DayT[] = [
+  { value: 'monday', label: 'Monday' },
+  { value: 'tuesday', label: 'Tuesday' },
+  { value: 'wednesday', label: 'Wednesday' },
+  { value: 'thursday', label: 'Thursday' },
+  { value: 'friday', label: 'Friday' },
+  { value: 'saturday', label: 'Saturday' },
+  { value: 'sunday', label: 'Sunday' },
+];
+
 export default function AvailableDayPicker() {
+  const selections = daysOfWeek.map(day => {
+    return createDaySelection(day);
+  });
+
   return (
     <div>
-      <ToggleGroup variant='outline' type='multiple'>
-        <ToggleGroupItem value='monday' aria-label='Toggle monday'>
-          <span>Monday</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem value='tuesday' aria-label='Toggle tuesday'>
-          <span>Tuesday</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem value='wednesday' aria-label='Toggle wednesday'>
-          <span>Wednesday</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem value='thursday' aria-label='Toggle thursday'>
-          <span>Thursday</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem value='friday' aria-label='Toggle friday'>
-          <span>Friday</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem value='saturday' aria-label='Toggle saturday'>
-          <span>Saturday</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem value='sunday' aria-label='Toggle sunday'>
-          <span>Sunday</span>
-        </ToggleGroupItem>
+      <ToggleGroup variant='outline' type='multiple' className=''>
+        {selections}
       </ToggleGroup>
     </div>
+  );
+}
+
+function createDaySelection(day: DayT) {
+  return (
+    <ToggleGroupItem value={day.value} aria-label={`Toggle ${day.value}`} className='p-4'>
+      <span>{day.label}</span>
+    </ToggleGroupItem>
   );
 }
--- client/src/components/technician/AvailableTimePicker.tsx
+++ client/src/components/technician/AvailableTimePicker.tsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 
-export default function AvailableTimePicker() {
+const currentDate = new Date();
+
+const mockTimeSlots: { date: Date }[] = [
+  {
+    date: currentDate,
+  },
+  {
+    date: new Date(new Date().setHours(currentDate.getHours() + 1)),
+  },
+  {
+    date: new Date(new Date().setHours(currentDate.getHours() + 2)),
+  },
+];
+
+type TimeSlotT = {
+  date: Date;
+};
+
+type AvailableTimePickerPropT = {
+  timeSlots: TimeSlotT[];
+};
+
+export default function AvailableTimePicker({
+  timeSlots = mockTimeSlots,
+}: AvailableTimePickerPropT) {
+  const timeSelections = timeSlots.map(slot => {
+    return timeSelection(slot.date);
+  });
+
+  return <div>{timeSelections}</div>;
+}
+
+function timeSelection(time: Date) {
+  const timeString = time.toLocaleTimeString();
+  time.setMinutes(0);
+
+  const hour = timeString.split(':').slice(0, 2).join(':');
+  const amPm = timeString.split(' ')[1];
+
   return (
-    <div>
-      <ToggleGroup variant='outline' type='multiple'>
-        <ToggleGroupItem value='bold' aria-label='Toggle bold'>
-          monday
-        </ToggleGroupItem>
-        <ToggleGroupItem value='italic' aria-label='Toggle italic'>
-          monday
-        </ToggleGroupItem>
-        <ToggleGroupItem value='strikethrough' aria-label='Toggle strikethrough'>
-          monday
-        </ToggleGroupItem>
-      </ToggleGroup>
-    </div>
+    <ToggleGroup variant='outline' type='multiple'>
+      <ToggleGroupItem value={hour} aria-label='Toggle '>
+        <span>{hour}</span>
+        <span>{amPm}</span>
+      </ToggleGroupItem>
+    </ToggleGroup>
   );
 }
