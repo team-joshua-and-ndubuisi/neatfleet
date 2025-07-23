f35ce9825d82738ecec97314c4f0a5b0d7b46181
jcloud <cloudsprogramming@gmail.com>
2025-07-23 01:13:57 -0700

    feat(db): add create Address for user

--- server/scripts/testAddresses.ts
+++ server/scripts/testAddresses.ts
@@ -1,8 +1,7 @@
 // scripts/testUserQueries.ts
 
 import { createUser } from '../src/services/userService';
-
-import { setUserAsAdmin, isAdmin } from '../src/services/adminService';
+import { createAddress } from '../src/services/addressService';
 
 import prisma from '../src/config/prisma';
 
@@ -14,10 +13,20 @@ const userCreationObj = {
   password: 'hashed_pw_123',
 };
 
+const addressCreationObj = {
+  userId: '',
+  street: 'main st',
+  city: 'south park',
+  state: 'CO',
+  zip: '54321',
+};
+
 async function main() {
   //Set up, create user
   const user = await createUser(userCreationObj);
   //Test 1: create an address for the user
+  addressCreationObj.userId = user.id;
+  const address = await createAddress(addressCreationObj);
 
   //Test 2: get a user's address by user ID
 }
---
+++ server/src/services/addressService.ts
@@ -0,0 +1,38 @@
+import prismaClient from '../config/prisma'; // Ensure your db connection is set up correctly
+
+const createAddress = async ({
+  userId,
+  street,
+  city,
+  state,
+  zip,
+  latitude,
+  longitude,
+}: {
+  userId: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  latitude?: number;
+  longitude?: number;
+}) => {
+  try {
+    const address = await prismaClient.address.create({
+      data: {
+        user_id: userId,
+        street,
+        city,
+        state,
+        zip,
+        latitude,
+        longitude,
+      },
+    });
+    return address;
+  } catch (error: any) {
+    throw new Error(`Error creating user: ${error.message}`);
+  }
+};
+
+export { createAddress };
