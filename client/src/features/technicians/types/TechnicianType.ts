4563d79ddd2f78e97ec6cf75378b4fd80e4de64e
Andrew Smith <meuzishun@gmail.com>
2025-07-24 18:19:19 -0400

    Create technicians feature

---
+++ client/src/features/technicians/types/TechnicianType.ts
@@ -0,0 +1,12 @@
+export interface Technician {
+  id: string;
+  user_id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  password: string;
+  created_at: string;
+  is_active: boolean;
+  current_rating: number;
+}
---
+++ client/src/features/technicians/types/index.ts
@@ -0,0 +1,1 @@
+export type { Technician } from './TechnicianType';
