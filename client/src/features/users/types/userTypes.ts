15a7330c2ecbfc18be30511dbd32305f7da6cb8f
Andrew Smith <meuzishun@gmail.com>
2025-07-24 18:16:42 -0400

    Modify userTypes to mirror mocked data

--- client/src/features/users/types/userTypes.ts
+++ client/src/features/users/types/userTypes.ts
@@ -1,5 +1,10 @@
 export interface UserType {
-  id: number;
-  name: string;
+  id: string;
+  first_name: string;
+  last_name: string;
   email: string;
+  phone: string;
+  password: string;
+  created_at: string;
+  is_active: boolean;
 }
