b1d62840cc2877072fca2d13afc206e42fb7f7a8
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2025-07-26 20:18:04 -0500

    fix color classes client/src/pages/ServiceCatalogPage.tsx

--- client/src/pages/ServiceCatalogPage.tsx
+++ client/src/pages/ServiceCatalogPage.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const servicesData = [
-  'Vacuuming',
-  'Painting',
-  'Lawn mowing',
-  'Clogged sink',
-  'Roof repair',
-];
+const servicesData = ['Vacuuming', 'Painting', 'Lawn mowing', 'Clogged sink', 'Roof repair'];
 
 interface ServiceSelectionProps {
   service: string;
@@ -15,7 +9,7 @@ interface ServiceSelectionProps {
 
 const ServiceSelection: React.FC<ServiceSelectionProps> = ({ service }) => {
   return (
-    <div className='bg-blue-400 text-white p-4 rounded-md flex items-center justify-center text-center'>
+    <div className='bg-primary text-white p-4 rounded-md flex items-center justify-center text-center'>
       {service}
     </div>
   );
@@ -26,7 +20,7 @@ const ServiceCatalogPage: React.FC = () => {
     <div className='mt-16 px-8'>
       <h4 className='text-3xl mb-4'>Service Catalog Page</h4>
       <div className='grid grid-cols-3 grid-rows-2 gap-4 max-w-4xl'>
-        {servicesData.map((service) => (
+        {servicesData.map(service => (
           <ServiceSelection key={service} service={service} />
         ))}
       </div>
