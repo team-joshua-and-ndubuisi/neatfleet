59ef5ab4606dd3a31be40d3ea591cf962417383b
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2025-07-26 20:15:38 -0500

    update classnames client/src/components/profile/BookingSnippet.tsx

--- client/src/components/profile/BookingSnippet.tsx
+++ client/src/components/profile/BookingSnippet.tsx
@@ -1,48 +1,41 @@
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
-  
-  import {FunctionComponent, PropsWithChildren} from 'react';
-  
-  
-   const BookingSnippets: FunctionComponent<PropsWithChildren> =({children})=> {
-    return (
-      <div>
-          <div className="w-full px-10 py-10">
-            <div>
-            <Accordion type="single" collapsible>
-              <AccordionItem    className="px-5 py-5" value="item-1">
-                <AccordionTrigger className="bg-stone-300 border-3 border border-black ">
-                  <h3 className="px-3 text-3xl" >
-                    Scheduled Bookings
-                  </h3>
-                </AccordionTrigger>
-                <AccordionContent className="flex justify-center">
-                 {children}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-            </div>
-            <div>
-            <Accordion type="single" collapsible>
-              <AccordionItem  className="px-5 py-5" value="item-1">
-                <AccordionTrigger className="bg-stone-300 border-3 border border-black">
-                  <h3 className="px-3 text-3xl" >
-                    Past Bookings
-                  </h3>
-                </AccordionTrigger>
-                <AccordionContent className="flex justify-center">
-                  {/* Child is map of booking car */}
-                    {children}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-            </div>
-          </div>
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+import { FunctionComponent, PropsWithChildren } from 'react';
+
+const BookingSnippets: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  return (
+    <div>
+      <div className='w-full px-10 py-10'>
+        <div>
+          <Accordion type='single' collapsible>
+            <AccordionItem className='px-5 py-5' value='item-1'>
+              <AccordionTrigger className='bg-primary-200 border-3 border-black '>
+                <h3 className='px-3 text-3xl'>Scheduled Bookings</h3>
+              </AccordionTrigger>
+              <AccordionContent className='flex justify-center'>{children}</AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <div>
+          <Accordion type='single' collapsible>
+            <AccordionItem className='px-5 py-5' value='item-1'>
+              <AccordionTrigger className='bg-primary-200 border-3 border-black'>
+                <h3 className='px-3 text-3xl'>Past Bookings</h3>
+              </AccordionTrigger>
+              <AccordionContent className='flex justify-center'>
+                {/* Child is map of booking car */}
+                {children}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
-    )
-  }
-  export default BookingSnippets
+    </div>
+  );
+};
+export default BookingSnippets;
